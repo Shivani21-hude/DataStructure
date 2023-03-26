21dcc594972d1d3454b2c9724fd3cc5bce797e5a
Shivani21-hude <shivani21hude@gmail.com>
2023-03-26 21:41:53 +0530

    [Shivani]-Custom Queue DataStructure

---
+++ DataStructure/Custom_Queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Custom_Queue
+    {
+        public Node head;
+        public void Queue(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = this.head;
+                while(temp.next!= null)
+                {
+                    temp = temp.next;               
+                }
+                 temp.next=node;
+            }
+            Console.WriteLine("{0} is inserted into Queue", data);
+        }
+
+        public void Display()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data+ " ");
+                    temp = temp.next;
+                }
+                
+            }
+        }
+
+        public void Dequeue()
+        {
+            Console.WriteLine("\n ==> Dequeue last element ");
+            if(this.head == null)
+            {
+                Console.WriteLine("Queue is empty ");
+            }
+            else
+            {
+                Node temp=this.head;
+                while (temp.next.next != null)             //56 30 70...  after 30 next.next is null so condtion becomes false 
+                {                                                        // so it wll not go for 70
+                   temp=temp.next;                        
+                  
+                }
+                temp.next = null;               
+            }
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -14,7 +14,7 @@ namespace DataStructure
             Console.WriteLine("*** Linked list ***");
             CustomLinkedList list = new CustomLinkedList();
             Console.WriteLine("Take 1 from given : \n" +
-                " 1) Custom Linked List \n 2) Custom Stack DataSturucture");
+                " 1) Custom Linked List \n 2) Custom Stack DataSturucture \n 3) Custom Queue dataStructure");
             int select = Convert.ToInt32(Console.ReadLine());
             switch (select)
             {
@@ -30,7 +30,7 @@ namespace DataStructure
                     break;
 
                 case 2:
-                    Console.WriteLine(" *** Stack DAtaStructure ***");
+                    Console.WriteLine("\n *** Stack DAtaStructure ***");
                     CustomStack stack = new CustomStack();
                     stack.Push(56);
                     stack.Push(30);
@@ -40,6 +40,17 @@ namespace DataStructure
                     stack.Display();
                     stack.Peek();
                     stack.Display();
+                    break;
+
+                case 3:
+                    Console.WriteLine("\n *** Queue DataStructure ***");
+                    Custom_Queue queue = new Custom_Queue();
+                    queue.Queue(56);
+                    queue.Queue(30);
+                    queue.Queue(70);
+                    queue.Display();
+                    queue.Dequeue();
+                    queue.Display();
                     break;
 
             }
